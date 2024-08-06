d2c829f2095a4d26d1c89a54195ee69129110e25
LehMipo412 <barbetzalel412@gmail.com>
2024-08-06 16:13:13 +0300

    All Of The Winning Conditions Works Exept the 3th coloum streak doesnt work

--- TicTaCommand/Assets/Scripts/Judge.cs
+++ TicTaCommand/Assets/Scripts/Judge.cs
@@ -11,9 +11,25 @@ public class Judge : MonoBehaviour
         var xSpriteRef = turnHandler._spriteSwapper.XSpriteToChange;
         var oSpriteRef = turnHandler._spriteSwapper.OSpriteToChange;
 
-        for (int i = 0; i < arrayRef.Length; i++)
+        if (arrayRef[0].image.sprite == xSpriteRef && arrayRef[1].image.sprite == xSpriteRef && arrayRef[2].image.sprite == xSpriteRef)
         {
-            if (i + 2 % 2 == 0)
+            turnHandler._textChanger.DeclareWinner(0);
+            arrayRef[0].image.color = Color.yellow;
+            arrayRef[1].image.color = Color.yellow;
+            arrayRef[2].image.color = Color.yellow;
+        }
+
+        if (arrayRef[0].image.sprite == oSpriteRef && arrayRef[1].image.sprite == oSpriteRef && arrayRef[2].image.sprite == oSpriteRef)
+        {
+            turnHandler._textChanger.DeclareWinner(1);
+            arrayRef[0].image.color = Color.yellow;
+            arrayRef[1].image.color = Color.yellow;
+            arrayRef[2].image.color = Color.yellow;
+        }
+
+        for (int i = 3; i < arrayRef.Length; i++)
+        {
+            if (i % 3 == 0)
             {
                 if (arrayRef[i].image.sprite == xSpriteRef && arrayRef[i + 1].image.sprite == xSpriteRef && arrayRef[i + 2].image.sprite == xSpriteRef)
                 {
@@ -31,6 +47,23 @@ public class Judge : MonoBehaviour
                 }
             }
 
+        }
+        for (int i = 0; i < 2; i++)
+        {
+            if (arrayRef[i].image.sprite == xSpriteRef && arrayRef[i + 3].image.sprite == xSpriteRef && arrayRef[i + 6].image.sprite == xSpriteRef)
+            {
+                turnHandler._textChanger.DeclareWinner(0);
+                arrayRef[i].image.color = Color.yellow;
+                arrayRef[i + 3].image.color = Color.yellow;
+                arrayRef[i + 6].image.color = Color.yellow;
+            }
+            if (arrayRef[i].image.sprite == oSpriteRef && arrayRef[i + 3].image.sprite == oSpriteRef && arrayRef[i + 6].image.sprite == oSpriteRef)
+            {
+                turnHandler._textChanger.DeclareWinner(1);
+                arrayRef[i].image.color = Color.yellow;
+                arrayRef[i + 3].image.color = Color.yellow;
+                arrayRef[i + 6].image.color = Color.yellow;
+            }
         }
         if (arrayRef[0].image.sprite == xSpriteRef && arrayRef[4].image.sprite == xSpriteRef && arrayRef[8].image.sprite == xSpriteRef)
         {
