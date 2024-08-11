54920b10cdae2d10f18ac0ebf5a726049f4d2b7b
MatrixVVhite <yotamoscohen@gmail.com>
2024-08-11 19:03:41 +0300

    Enemy can now hit player, and the UI updates

--- Solid's Adventure/Assets/Scripts/GameManager.cs
+++ Solid's Adventure/Assets/Scripts/GameManager.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 public class GameManager : MonoBehaviour
 {
     [SerializeField] public GameObject PlayerGO;
+    [SerializeField] public PlayerUI PlayerHUD;
+    [SerializeField] public PlayerHealth PlayerHP;
     //public List<GameObject> EnemyList = new List<GameObject>(); //use this in case we need to count enemies
 
     public static GameManager Instance { get; private set; }
@@ -21,5 +23,20 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    public void EnemyHitPlayer()
+    {
+        Debug.Log("Enemy hit player");
+        PlayerHP.TakeDamage(5);
+    }
 
+    public void OnPlayerTakeDamage(float newCurrentHP, float maxHP)
+    {
+        Debug.Log($"Player max HP = {maxHP}. Player current HP = {newCurrentHP}");
+        PlayerHUD.DisplayHP(newCurrentHP / maxHP);
+    }
+
+    public void OnPlayerDeath()
+    {
+        //Game over
+    }
 }
--- Solid's Adventure/Assets/Scripts/PlayerCollision.cs
+++ Solid's Adventure/Assets/Scripts/PlayerCollision.cs
@@ -5,12 +5,16 @@ public class PlayerCollision : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.CompareTag("Pickup"))
+        if (collision.gameObject.CompareTag("Pickup"))
         {
 
             Debug.Log("I Should Notify The Subscribers");
 
         }
-        
+        else if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Debug.Log("Enemy collided with player");
+            GameManager.Instance.EnemyHitPlayer();
+        }
     }
 }
